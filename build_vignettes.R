e7a80e0b794c64c4ea5772b952c8d87083765c1f
Zuguang Gu <jokergoo@gmail.com>
2021-07-10 13:38:59 +0200

    first commit

---
+++ build_vignettes.R
@@ -0,0 +1,8 @@
+library(rmarkdown)
+render("cola.Rmd")
+render("cola_quick.Rmd")
+render("functional_enrichment.Rmd")
+render("predict.Rmd")
+render("work_with_big_datasets.Rmd")
+render("hierarchical.Rmd")
+
